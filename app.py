756e8c763a486adc2b7e9f9c7a163a2fcc282ede
Mike Yang <perryvm06vm06@gmail.com>
2021-04-07 15:39:29 +0000

    feat(app): add error handlers for http status code

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from tasks.views import task_bp
+from utils.enums import HttpStatusCode
 
 app = Flask(__name__)
+
+# register blueprints
 app.register_blueprint(task_bp)
+
+# configurations
 app.url_map.strict_slashes = False
+
+
+# error handlers
+@app.errorhandler(HttpStatusCode.InternalError.value)
+def occur_internal_error(e):
+    error_msg = str(e) or 'Internal Error'
+    return jsonify(error=error_msg), HttpStatusCode.InternalError.value
+
+
+@app.errorhandler(HttpStatusCode.NotFound.value)
+def resource_not_found(e):
+    error_msg = str(e) or 'Resource Not Found'
+    return jsonify(error=error_msg), HttpStatusCode.NotFound.value
+
+
+@app.errorhandler(HttpStatusCode.BadRequest.value)
+def bad_request(e):
+    error_msg = str(e) or 'Bad Request'
+    return jsonify(error=error_msg), HttpStatusCode.BadRequest.value

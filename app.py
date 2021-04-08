f954a702f581e9ed143eb8866089e02b19804b7d
Mike Yang <perryvm06vm06@gmail.com>
2021-04-08 10:23:54 +0000

    build(pytest): add pytest configuration

--- app.py
+++ app.py
@@ -3,29 +3,33 @@ from flask import Flask, jsonify
 from tasks.views import task_bp
 from utils.enums import HttpStatusCode
 
-app = Flask(__name__)
 
-# register blueprints
-app.register_blueprint(task_bp)
+def create_app():
+    app = Flask(__name__)
 
-# configurations
-app.url_map.strict_slashes = False
+    # register blueprints
+    app.register_blueprint(task_bp)
+
+    # configurations
+    app.url_map.strict_slashes = False
+
+    # error handlers
+    @app.errorhandler(HttpStatusCode.InternalError.value)
+    def occur_internal_error(e):
+        error_msg = str(e) or 'Internal Error'
+        return jsonify(error=error_msg), HttpStatusCode.InternalError.value
+
+    @app.errorhandler(HttpStatusCode.NotFound.value)
+    def resource_not_found(e):
+        error_msg = str(e) or 'Resource Not Found'
+        return jsonify(error=error_msg), HttpStatusCode.NotFound.value
+
+    @app.errorhandler(HttpStatusCode.BadRequest.value)
+    def bad_request(e):
+        error_msg = str(e) or 'Bad Request'
+        return jsonify(error=error_msg), HttpStatusCode.BadRequest.value
+
+    return app
 
 
-# error handlers
-@app.errorhandler(HttpStatusCode.InternalError.value)
-def occur_internal_error(e):
-    error_msg = str(e) or 'Internal Error'
-    return jsonify(error=error_msg), HttpStatusCode.InternalError.value
-
-
-@app.errorhandler(HttpStatusCode.NotFound.value)
-def resource_not_found(e):
-    error_msg = str(e) or 'Resource Not Found'
-    return jsonify(error=error_msg), HttpStatusCode.NotFound.value
-
-
-@app.errorhandler(HttpStatusCode.BadRequest.value)
-def bad_request(e):
-    error_msg = str(e) or 'Bad Request'
-    return jsonify(error=error_msg), HttpStatusCode.BadRequest.value
+app = create_app()
